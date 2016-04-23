911e77f333debdfc0fabafc2d4e908ca09a41d5d
rcj1492 <richard@collectiveacuity.com>
2016-04-23 15:01:16 -0700

    added model pages;

--- server/launch.py
+++ server/launch.py
@@ -17,7 +17,7 @@ def dashboard_page():
     app.logger.debug('Headers: %s' % request_dict)
     return render_template('landing.html'), 200
 
-@app.route('/operation/put', methods=['POST'])
+@app.route('/v1/operation/model')
 def operation_validate():
     valid_response, code = operationResource.request.validate(request)
     if code:
@@ -26,8 +26,35 @@ def operation_validate():
     for key, value in request.headers.items():
         request_dict['headers'][key] = value
     app.logger.debug('Headers: %s' % request_dict)
-    reply_dict = operationModel.put(request_dict)
-    return jsonify(reply_dict), 200
+    model_details = operationResource.model.schema
+    model_title = operationResource.model.title
+    return render_template('model.html', modelTitle=model_title, modelDetails=model_details), 200
+
+@app.route('/v1/catch/model')
+def operation_validate():
+    valid_response, code = operationResource.request.validate(request)
+    if code:
+        return jsonify(valid_response), code
+    request_dict = {'headers': {}, 'body': request.form}
+    for key, value in request.headers.items():
+        request_dict['headers'][key] = value
+    app.logger.debug('Headers: %s' % request_dict)
+    model_details = operationResource.model.schema
+    model_title = operationResource.model.title
+    return render_template('model.html', modelTitle=model_title, modelDetails=model_details), 200
+
+@app.route('/v1/specimen/model')
+def operation_validate():
+    valid_response, code = operationResource.request.validate(request)
+    if code:
+        return jsonify(valid_response), code
+    request_dict = {'headers': {}, 'body': request.form}
+    for key, value in request.headers.items():
+        request_dict['headers'][key] = value
+    app.logger.debug('Headers: %s' % request_dict)
+    model_details = operationResource.model.schema
+    model_title = operationResource.model.title
+    return render_template('model.html', modelTitle=model_title, modelDetails=model_details), 200
 
 @app.errorhandler(404)
 def page_not_found(error):
